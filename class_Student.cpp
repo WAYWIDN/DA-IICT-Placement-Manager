f2fcab3cf433b03c22e569bbe39aabdfc316919b
Vivek00021 <vivekparmar00021@gmail.com>
2024-02-20 22:45:57 +0530

    Create class_Student.cpp

---
+++ class_Student.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+    using namespace std;
+
+class Student
+{
+    private :
+
+    char id[8];
+    string name;
+    int batch;
+    char program[8];
+    int written_test;
+    int interview;
+    int shortlisted;
+    int job_offers;
+    string Company;
+    string Place;
+    float package;
+
+    public :
+
+
+
+
+};
